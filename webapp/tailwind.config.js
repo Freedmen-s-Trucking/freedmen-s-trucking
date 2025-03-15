12f81e727c04389da2022e02a27e38e18ccb9261
Tadjaur <tadjaur@tas-groups.com>
2025-03-15 02:37:44 +0100

    update the freedman webapp

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,20 +1,10 @@
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/preline/preline.js",
-    // ...
-    flowbite.content(),
-  ],
-  plugins: [
-    require("preline/plugin"),
-    // ...
-    flowbite.plugin(),
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
+  plugins: [flowbite.plugin()],
 };
