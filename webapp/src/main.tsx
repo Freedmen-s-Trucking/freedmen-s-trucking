569dd0ab7bff67223457265dea3db17d689037ee
GHULAM-1 <ilyasghulam35@gmail.com>
2025-04-13 21:14:56 +0500

    fixing css

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./route-tree.gen";
+import "./index.css";
+import { RootProviders } from "./provider/providers";
+import { useAppSelector } from "./stores/hooks";
+import { useGetRemoteConfig } from "./hooks/use-remote-config";
+import { RemoteConfigKeys } from "./utils/constants";
+// import { useAuth } from "./hooks/use-auth";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  context: { user: null, remoteConfigs: {} },
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export const CustomRouter: React.FC = () => {
+  const { user } = useAppSelector((state) => state.authCtrl);
+  const canShowPreviewLandingPage = useGetRemoteConfig(
+    RemoteConfigKeys.can_show_preview_landing_page,
+  );
+
+  return (
+    <RouterProvider
+      router={router}
+      context={{ user, remoteConfigs: { canShowPreviewLandingPage } }}
+    />
+  );
+};
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RootProviders>
+        <CustomRouter />
+      </RootProviders>
+    </StrictMode>,
+  );
+}
