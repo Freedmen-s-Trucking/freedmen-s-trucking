5418d23e7b625ba6395cce595360b3ec8b65f649
Tadjaur <tadjaur@tas-groups.com>
2025-04-25 12:32:32 +0100

    Setup Remote Config Deployment

---
+++ backends/remote-configs/.eslintrc.js
@@ -0,0 +1,34 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: [`${__dirname}/tsconfig.dev.json`],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+    "/generated/**/*", // Ignore generated files.
+  ],
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+    "@typescript-eslint/no-explicit-any": "off",
+    "space-before-function-paren": ["error", "never"],
+  },
+};
