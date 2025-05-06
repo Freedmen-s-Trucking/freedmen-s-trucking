de4a66f90f9475f2aab6d34d04b6219dea5ff680
Tadjaur <tadjaur@tas-groups.com>
2025-05-06 10:36:44 +0100

    fix negative finger print

--- webapp/src/utils/functions.ts
+++ webapp/src/utils/functions.ts
@@ -287,5 +287,5 @@ export function generateBrowserFingerprint() {
     hash = (hash << 5) - hash + char;
     hash = hash & hash; // Convert to 32bit integer
   }
-  return hash;
+  return Math.abs(hash);
 }
